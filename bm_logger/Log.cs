974a7ff73593dde1004cb7b808417b34cea6d2cd
fontchris <font.christopher@gmail.com>
2016-07-24 13:03:50 -0500

    Added logging documentation.

--- bm_logger/Log.cs
+++ bm_logger/Log.cs
@@ -4,15 +4,30 @@ using System.IO;
 
 namespace bm_logger
 {
+    /// <summary>
+    /// Logger class is a simple singleton to provide a unified interface to our log file + console.
+    /// </summary>
+    /// <remarks>
+    /// This class needs to be upgraded to support console, file or redirected only logging.
+    /// </remarks>
     public class Log
     {
+        /// <summary>
+        /// Logging level for writing log files.
+        /// </summary>
         public enum LogLevel
         {
-            Error, Warning, Debug, Info
+            /// <summary>Error log level. Should be reserved for faults/exceptions</summary>
+            Error,
+            /// <summary>Warning log level. Should be reserved for recovered errors or aborts.</summary>
+            Warning,
+            /// <summary>Debug log level. Should be reserved for any normal data needed for debugging issues.</summary>
+            Debug,
+            /// <summary>Info log level. Should be used for system state output and general messages.</summary>
+            Info
         };
 
         private static Log _instanceLog = null;
-        public LogLevel Level = LogLevel.Info;
         private readonly string _logFile;
 
         private Log()
@@ -20,8 +35,24 @@ namespace bm_logger
             _logFile = Environment.CurrentDirectory + "/" + DateTime.Today.ToString("hh_mm_dd_MM_yyyy") + ".log";
         }
 
+        /// <summary>
+        /// Current logging level. Please set this to a lower level to disable some other messages.
+        /// </summary>
+        /// <remarks>
+        /// This needs to be updated to support module specific log levels. For now, it is global.
+        /// </remarks>
+        public LogLevel Level = LogLevel.Info;
+
+        /// <summary>
+        /// Singleton instance accessor.
+        /// </summary>
         public static Log Instance => _instanceLog ?? (_instanceLog = new Log());
 
+        /// <summary>
+        /// Log a message through this function. TODO: Add log level specific helpers.
+        /// </summary>
+        /// <param name="logMsg">Message to log. Needs to be a string.</param>
+        /// <param name="level">Logging level to use for output.</param>
         public void Message(string logMsg, LogLevel level)
         {
             if (level > Level)
